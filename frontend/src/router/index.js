5f4bb7d46b410e6a5e417c7e6dbaa497239ba180
carlrc <carl.richmond@pm.me>
2025-07-15 14:33:25 +0200

    work

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import PlayersPage from '../views/PlayersPage.vue'
 import CharactersPage from '../views/CharactersPage.vue'
 import MemoriesPage from '../views/MemoriesPage.vue'
+import EncountersPage from '../views/EncountersPage.vue'
 
 const routes = [
     {
@@ -22,6 +23,11 @@ const routes = [
         path: '/memories',
         name: 'Memories',
         component: MemoriesPage
+    },
+    {
+        path: '/encounters',
+        name: 'Encounters',
+        component: EncountersPage
     }
 ]
 
