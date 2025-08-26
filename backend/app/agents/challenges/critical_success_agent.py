5abf53e6da1fec82d3096a54fedddde4667f52c8
carlrc <carl.richmond@pm.me>
2025-08-26 18:44:03 +0200

    work

--- backend/app/agents/challenges/critical_success_agent.py
+++ backend/app/agents/challenges/critical_success_agent.py
@@ -1,65 +1,21 @@
 import logging
-from typing import List
 
 from langfuse import observe as langfuse_observe
-from pydantic_ai import Agent, RunContext, UnexpectedModelBehavior
+from pydantic_ai import UnexpectedModelBehavior
 from pydantic_ai.agent import AgentRunResult
-from pydantic_ai.models.openai import OpenAIModel
 
 from app.agents.agent_output import StandardAgentOutput
 from app.agents.base_agent import BaseAgent
 from app.agents.challenges.dependencies import ChallengeAgentDeps
-from app.agents.prompts.utils import (
-    structure_character_memories,
-    structure_filtered_reveal_content,
-)
-from app.models.character import Character
-from app.models.memory import Memory
-from app.models.player import Player
 
 logger = logging.getLogger(__name__)
 
 
 class CriticalSuccessAgent(BaseAgent):
-    def __init__(
-        self,
-        character: Character,
-        player: Player,
-        system_prompt: str,
-        memories: List[Memory],
-        reveals: List[str],
-    ):
+    def __init__(self, system_prompt: str):
         super().__init__()
-        self.reveals = reveals
-        agent = Agent(
-            OpenAIModel(
-                model_name="gpt-4o-mini",
-            ),
-            instructions=system_prompt
-            + "\n"
-            + character.to_prompt()
-            + "\n"
-            + structure_filtered_reveal_content(reveals=reveals)
-            + "\n"
-            + structure_character_memories(memories=memories, player=player),
-            history_processors=[self._keep_recent_messages],
-            retries=self.retries,
-            instrument=True,
-        )
+        self.agent = self._generate_agent(system_prompt=system_prompt, model_temp=0.6)
         self.run_result: AgentRunResult[StandardAgentOutput] = None
-
-        @agent.instructions
-        def add_encounter(ctx: RunContext[ChallengeAgentDeps]) -> str:
-            if ctx.deps.encounter.description:
-                return f"""# Physical Location Context
-                    Your character is currently in the following encounter. Use this information as your physical world context.
-                    {ctx.deps.encounter.description}
-                    """
-            else:
-                return ""
-
-        # Set instance variable after decorators defined
-        self.agent = agent
 
     @langfuse_observe
     async def chat(self, player_transcript: str, deps: ChallengeAgentDeps) -> str:
